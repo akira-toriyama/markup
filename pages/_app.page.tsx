503f583673346b4617fd75308329a1b3cb50aa80
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-12 22:53:09 +0900

    fix(other): :bug: fix fontawesome

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,9 +1,10 @@
 import "destyle.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import React from "react";
 import { ErrorBoundary } from "~/presenter/component/ErrorBoundary";
+fontawesomeConfig.autoAddCss = false;
 
 const Page: NextPage<AppProps> = (props) => (
   <ErrorBoundary>
