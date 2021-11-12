bc152342ac7b2dddbf26afa7245d928f6f7e65c5
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-12 22:31:32 +0900

    feat(presenter): :lipstick: update ch2

---
+++ src/presenter/component/Ch2/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Ch2Area = styled.article``;
+export const HeaderArea = styled.header``;
+export const Section1Area = styled.section``;
+export const Section2Area = styled.section``;
+export const Section3Area = styled.section``;
+export const Footer = styled.footer``;
