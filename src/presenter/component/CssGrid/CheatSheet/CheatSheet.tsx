a65c4c23d61f3d47c76f566d929a0b7bad2953bc
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-15 21:58:45 +0900

    feat(presenter): :sparkles: update css grid

--- src/presenter/component/CssGrid/CheatSheet/CheatSheet.tsx
+++ src/presenter/component/CssGrid/CheatSheet/CheatSheet.tsx
@@ -2,13 +2,209 @@ import React from "react";
 import * as S from "./styles";
 import { randomColor } from "~/presenter/lib/randomColor";
 
+const CssGrid1: React.VFC = () => (
+  <S.Container1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+    <S.Item1 color={randomColor()}>
+      <S.Txt>CssGrid1</S.Txt>
+    </S.Item1>
+  </S.Container1>
+);
+
+const CssGrid2: React.VFC = () => (
+  <S.Container2>
+    <S.Item2 color={randomColor()}>
+      <S.Txt>CssGrid2</S.Txt>
+    </S.Item2>
+    <S.Item2 color={randomColor()}>
+      <S.Txt>CssGrid2</S.Txt>
+    </S.Item2>
+    <S.Item2 color={randomColor()}>
+      <S.Txt>CssGrid2</S.Txt>
+    </S.Item2>
+  </S.Container2>
+);
+
+const CssGrid3: React.VFC = () => (
+  <S.Container3>
+    <S.Item3 color={randomColor()}>
+      <S.Txt>CssGrid3</S.Txt>
+    </S.Item3>
+    <S.Item3 color={randomColor()}>
+      <S.Txt>CssGrid3</S.Txt>
+    </S.Item3>
+    <S.Item3 color={randomColor()}>
+      <S.Txt>CssGrid3</S.Txt>
+    </S.Item3>
+  </S.Container3>
+);
+
+const CssGrid4: React.VFC = () => (
+  <>
+    <S.Container4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+    </S.Container4>
+
+    <S.Container4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+      <S.Item4 color={randomColor()}>
+        <S.Txt>CssGrid4</S.Txt>
+      </S.Item4>
+    </S.Container4>
+  </>
+);
+
+const CssGrid5: React.VFC = () => (
+  <S.Container5>
+    <S.Item5 color={randomColor()}>
+      <S.Txt>
+        CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5
+        CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5 CssGrid5
+      </S.Txt>
+    </S.Item5>
+    <S.Item5 color={randomColor()}>
+      <S.Txt>CssGrid5</S.Txt>
+    </S.Item5>
+    <S.Item5 color={randomColor()}>
+      <S.Txt>CssGrid5</S.Txt>
+    </S.Item5>
+  </S.Container5>
+);
+
+const CssGrid6: React.VFC = () => (
+  <S.Container6>
+    <S.Item6 color={randomColor()}>
+      <S.Txt>
+        CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6
+        CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6 CssGrid6
+      </S.Txt>
+    </S.Item6>
+    <S.Item6 color={randomColor()}>
+      <S.Txt>CssGrid6</S.Txt>
+    </S.Item6>
+    <S.Item6 color={randomColor()}>
+      <S.Txt>CssGrid6</S.Txt>
+    </S.Item6>
+  </S.Container6>
+);
+
+const CssGrid7: React.VFC = () => (
+  <>
+    <S.Container7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7</S.Txt>
+      </S.Item7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7</S.Txt>
+      </S.Item7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7</S.Txt>
+      </S.Item7>
+    </S.Container7>
+
+    <S.Container7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7 CssGrid7 CssGrid7 CssGrid7</S.Txt>
+      </S.Item7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7</S.Txt>
+      </S.Item7>
+      <S.Item7 color={randomColor()}>
+        <S.Txt>CssGrid7</S.Txt>
+      </S.Item7>
+    </S.Container7>
+  </>
+);
+
+const CssGrid8: React.VFC = () => (
+  <S.Container8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+    <S.Item8 color={randomColor()}>
+      <S.Txt>CssGrid8</S.Txt>
+    </S.Item8>
+  </S.Container8>
+);
+
+const CssGrid9: React.VFC = () => (
+  <S.Container9>
+    <S.Item9 color={randomColor()}>
+      <S.Txt>CssGrid9</S.Txt>
+    </S.Item9>
+    <S.Item9 color={randomColor()}>
+      <S.Txt>CssGrid9</S.Txt>
+    </S.Item9>
+    <S.Item9 color={randomColor()}>
+      <S.Txt>CssGrid9</S.Txt>
+    </S.Item9>
+    <S.Item9 color={randomColor()}>
+      <S.Txt>CssGrid9</S.Txt>
+    </S.Item9>
+  </S.Container9>
+);
+
 export const CheatSheet: React.VFC = () => (
-  <S.Area>
-    <S.Section color={randomColor()}>
-      <p>text</p>
-    </S.Section>
-    <S.Section color={randomColor()}>
-      <p>text</p>
-    </S.Section>
-  </S.Area>
+  <>
+    <CssGrid1 />
+    <CssGrid2 />
+    <CssGrid3 />
+    <CssGrid4 />
+    <CssGrid5 />
+    <CssGrid6 />
+    <CssGrid7 />
+    <CssGrid8 />
+    <CssGrid9 />
+  </>
 );
--- src/presenter/component/CssGrid/CheatSheet/styles.ts
+++ src/presenter/component/CssGrid/CheatSheet/styles.ts
@@ -1,13 +1,139 @@
 import styled from "styled-components";
+import { css } from "styled-components";
 
-export const Area = styled.div`
+export const Txt = styled.p`
+  color: black;
+  font-weight: bold;
+  margin: 0;
+  &::before {
+    color: white;
+    content: "(";
+  }
+  &::after {
+    color: white;
+    content: ")";
+  }
+`;
+
+type ItemProps = {
+  color: string;
+};
+
+const containerStyle = css`
   box-sizing: border-box;
   display: grid;
   border: 1px solid black;
+  margin: 30px;
 `;
 
-export const Section = styled.div<{ color: string }>`
+const itemStyle = css`
   box-sizing: border-box;
-  background-color: ${(props) => props.color};
   border: 1px solid black;
+`;
+
+export const Container1 = styled.div`
+  ${containerStyle}
+
+  grid-template-rows: 50px 100px 150px;
+  grid-template-columns: 150px 100px;
+`;
+
+export const Item1 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+export const Container2 = styled.div`
+  ${containerStyle}
+`;
+
+export const Item2 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+export const Container3 = styled.div`
+  ${containerStyle}
+  grid-template-columns: 50px 100px 1fr;
+`;
+
+export const Item3 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+export const Container4 = styled.div`
+  ${containerStyle}
+  /* 50px  1/6 50px 5/6 */
+  grid-template-columns: 50px 1fr 50px 5fr;
+`;
+
+export const Item4 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+/**
+ * Container6 と比較して
+ * 1fr と autoの違いを確認
+ */
+export const Container5 = styled.div`
+  ${containerStyle}
+  grid-template-columns: auto auto auto;
+`;
+
+export const Item5 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+export const Container6 = styled.div`
+  ${containerStyle}
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export const Item6 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+/**
+ * auto 1fr 1fr
+ * 1列目の内容が小さければ、残り2列は大きくなる
+ * 1列目の内容が大きければ、残り2列は小さくなる
+ */
+export const Container7 = styled.div`
+  ${containerStyle}
+  grid-template-columns: auto 1fr 1fr;
+`;
+
+export const Item7 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+/**
+ * repeatで繰り返し
+ */
+export const Container8 = styled.div`
+  ${containerStyle}
+  grid-template-columns: repeat(5, 1fr) auto;
+`;
+
+export const Item8 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
+`;
+
+/**
+ * repeatで繰り返し
+ */
+export const Container9 = styled.div`
+  ${containerStyle}
+  grid-template-columns: repeat(2, 50px 100px);
+`;
+
+export const Item9 = styled.div<ItemProps>`
+  ${itemStyle}
+  background-color: ${(props) => props.color};
 `;
