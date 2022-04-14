9c632c7dd4d35f1d14d14f445055b3ebe370cd52
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-04-14 20:30:33 +0900

    tweak(presenter): :recycle: fix comment

--- src/presenter/component/EveryLayout/CheatSheet/1/styles.ts
+++ src/presenter/component/EveryLayout/CheatSheet/1/styles.ts
@@ -1,18 +1,15 @@
 import styled from "styled-components";
 
-export const Contents = styled.div``;
-
-type ItemProps = {
+/**
+ * `box-sizing`に関して
+ * 予測がしやすいので、枠線を内側に書く
+ */
+export const Item = styled.div<{
   color: string;
-};
-const Item = styled.div<ItemProps>`
-  /* 枠線を内側に書く */
-  /* 予測がしやすい */
+}>`
   box-sizing: border-box;
   background-color: ${(props) => props.color};
-`;
 
-export const Items1 = styled(Item)`
   width: 100px;
   height: 100px;
   border: solid 1px black;
